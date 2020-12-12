4f2d624af8ad30025a13490b1bfa595864a7138a
Sterndata <subscribed-lists@sterndata.com>
2020-12-12 17:45:42 -0600

    initial commit

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'child-style', get_stylesheet_uri().'/style.css');
+}
