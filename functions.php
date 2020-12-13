8f6df38266cd7727d24348c8b17e38dec3dc65a7
Sterndata <subscribed-lists@sterndata.com>
2020-12-12 23:40:34 -0600

    reorder css includes, first stab at sidebar

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri().'/style.css');
+    $deps = array('twenty-twenty-one-style','twenty-twenty-one-print-style');
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.css', $deps );
 }
