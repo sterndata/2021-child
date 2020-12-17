488851be977c26380b0d3858272f2f1d9cfa9ad9
Sterndata <subscribed-lists@sterndata.com>
2020-12-17 15:45:33 -0600

    masthead background, move logo left & menu right

--- functions.php
+++ functions.php
@@ -4,3 +4,4 @@ function my_theme_enqueue_styles() {
     $deps = array('twenty-twenty-one-style','twenty-twenty-one-print-style');
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri().'/style.css', $deps );
 }
+
