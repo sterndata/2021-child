8736aef80b8d6337ae52d145cb1ed6aeaf227e9d
Sterndata <subscribed-lists@sterndata.com>
2020-12-17 17:16:44 -0600

    Add copyright to footer

---
+++ footer.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since 1.0.0
+ */
+
+?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- #content -->
+
+	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+
+		<?php if ( has_nav_menu( 'footer' ) ) : ?>
+			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
+				<ul class="footer-navigation-wrapper">
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'footer',
+							'items_wrap'     => '%3$s',
+							'container'      => false,
+							'depth'          => 1,
+							'link_before'    => '<span>',
+							'link_after'     => '</span>',
+							'fallback_cb'    => false,
+						)
+					);
+					?>
+				</ul><!-- .footer-navigation-wrapper -->
+			</nav><!-- .footer-navigation -->
+		<?php endif; ?>
+		<div class="site-info">
+			<div class="powered-by">
+				<?php
+				printf(
+					/* translators: %s: WordPress. */
+					esc_html__( 'Proudly powered by %s.', 'twentytwentyone' ),
+					'<a href="' . esc_attr__( 'https://wordpress.org/', 'twentytwentyone' ) . '">WordPress</a>'
+				);
+				?>
+			</div><!-- .powered-by -->
+            <div class="footer-copyright">
+            <?php
+            $copystr = get_theme_mod('copyright', ''); 
+				printf(
+                    /* translators: %s: WordPress. */
+                    
+					esc_html__( '&copy;'. date('Y') . ' %s', 'sds_2021-child' ),
+					$copystr
+				);
+				?>
+			</div><!-- .footer-copyright -->
+
+
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
